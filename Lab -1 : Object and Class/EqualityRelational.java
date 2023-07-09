547d71f279520800fbd18465b4321870e689cae4
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-07-09 22:23:13 +0300

    Create EqualityRelational.java

---
+++ Lab -1 : Object and Class/EqualityRelational.java
@@ -0,0 +1,12 @@
+public class EqualityRelational {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    System.out.println(a == b); // false
+    System.out.println(a != b); // true
+    System.out.println(a > b);  // false
+    System.out.println(a < b);  // true
+    System.out.println(b >= a); // true
+    System.out.println(b <= a); // false
+  }
+}
