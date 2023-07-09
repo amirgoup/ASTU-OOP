bf4799307b14e52f0bbfceefd5a59585979f5621
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-07-09 21:54:42 +0300

    Create EscapeSequenceCharacters.java
    The output of this program will be:
    
    Hello   World
    Hello
    World
    Worldo
    Hello"World"
    Hello'World'
    Hello\World
    HellWorld

---
+++ Lab -1 : Object and Class/EscapeSequenceCharacters.java
@@ -0,0 +1,12 @@
+public class EscapeSequenceCharacters {
+    public static void main(String[] args) {
+        // Using escape sequence characters
+        System.out.println("Hello\tWorld");
+        System.out.println("Hello\nWorld");
+        System.out.println("Hello\rWorld");
+        System.out.println("Hello\"World\"");
+        System.out.println("Hello\'World\'");
+        System.out.println("Hello\\World");
+        System.out.println("Hello\bWorld");
+    }
+}
