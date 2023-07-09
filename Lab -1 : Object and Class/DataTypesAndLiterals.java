a5f1bc9b9ad3c1c6487c7b0173ae50e5e2e417f2
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-07-09 21:49:14 +0300

    Create DataTypesAndLiterals.java
    In this program, we have used different types of literals such as integer, floating point, character, and boolean. We have also declared variables of different data types such as byte, short, int, long, float, double, char, and boolean. The output of this program will be:
    
    Byte Literal: 100
    Short Literal: 10000
    Integer Literal: 100000
    Long Literal: 1000000000
    Floating Point Literal: 10.5
    Double Literal: 10.5
    Character Literal: a
    Boolean Literal: true

---
+++ Lab -1 : Object and Class/DataTypesAndLiterals.java
@@ -0,0 +1,28 @@
+public class DataTypesAndLiterals {
+    public static void main(String[] args) {
+        // Integer literals
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 1000000000L;
+
+        // Floating point literals
+        float f = 10.5f;
+        double d = 10.5;
+
+        // Character literal
+        char c = 'a';
+
+        // Boolean literal
+        boolean bool = true;
+
+        System.out.println("Byte Literal: " + b);
+        System.out.println("Short Literal: " + s);
+        System.out.println("Integer Literal: " + i);
+        System.out.println("Long Literal: " + l);
+        System.out.println("Floating Point Literal: " + f);
+        System.out.println("Double Literal: " + d);
+        System.out.println("Character Literal: " + c);
+        System.out.println("Boolean Literal: " + bool);
+    }
+}
