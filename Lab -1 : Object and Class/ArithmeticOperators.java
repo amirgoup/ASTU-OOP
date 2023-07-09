04fdc234a7f060a95f90ba85213b9e6abf841f9c
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-07-09 22:11:51 +0300

    Create ArithmeticOperators.java

---
+++ Lab -1 : Object and Class/ArithmeticOperators.java
@@ -0,0 +1,26 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Addition operator
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+
+        // Subtraction operator
+        int difference = b - a;
+        System.out.println("Difference: " + difference);
+
+        // Multiplication operator
+        int product = a * b;
+        System.out.println("Product: " + product);
+
+        // Division operator
+        int quotient = b / a;
+        System.out.println("Quotient: " + quotient);
+
+        // Modulus operator
+        int remainder = b % a;
+        System.out.println("Remainder: " + remainder);
+    }
+}
