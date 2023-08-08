39fad4b894164d0c4291600cf2bd0c9d1b7e07a3
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-08-08 12:01:45 +0300

    Create BreakContinue.java

---
+++ Lab -1 : Object and Class/Control flow structures/BreakContinue.java
@@ -0,0 +1,26 @@
+public class BreakContinueExample {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i < 5) {
+            if (i == 3) {
+                System.out.println("Found number 3 at index " + i);
+                break;
+            }
+            System.out.println("Number " + i);
+            i++;
+        }
+
+        int j = 0;
+
+        while (j < 5) {
+            if (j == 3) {
+                System.out.println("Skipping number 3 at index " + j);
+                j++;
+                continue;
+            }
+            System.out.println("Number " + j);
+            j++;
+        }
+    }
+}
