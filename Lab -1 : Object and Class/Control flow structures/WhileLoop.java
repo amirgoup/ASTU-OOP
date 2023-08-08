56bfda8b9932a771e1d805f18271b85e4f2d923c
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-08-08 11:42:38 +0300

    Create WhileLoop.java

---
+++ Lab -1 : Object and Class/Control flow structures/WhileLoop.java
@@ -0,0 +1,10 @@
+// Java program to illustrate while loop
+class WhileLoop {
+    public static void main(String args[]) {
+        int x = 1;
+        while (x <= 5) {
+            System.out.println("Value of x:" + x);
+            x++;
+        }
+    }
+}
