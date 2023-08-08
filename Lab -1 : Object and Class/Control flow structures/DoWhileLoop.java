31cd8948cbc86dff1eadf33e3715cdb83fe1694e
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-08-08 11:43:49 +0300

    Create DoWhileLoop.java

---
+++ Lab -1 : Object and Class/Control flow structures/DoWhileLoop.java
@@ -0,0 +1,10 @@
+// Java program to illustrate do-while loop
+class DoWhileLoop {
+    public static void main(String args[]) {
+        int x = 1;
+        do {
+            System.out.println("Value of x:" + x);
+            x++;
+        } while (x <= 5);
+    }
+}
