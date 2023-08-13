8fabfe2e32f521119522d0b1c63f3d5a564adcc1
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-08-13 06:08:21 +0300

    Update Car.java

--- Lab -1 : Object and Class/Classes and Objects/1/Car.java
+++ Lab -1 : Object and Class/Classes and Objects/1/Car.java
@@ -1,1 +1,16 @@
+class Car {
+  String color;
+  int year;
+  String make;
+}
+
+public class Main {
+  public static void main(String[] args) {
+    Car myCar = new Car();
+    myCar.color = "red";
+    myCar.year = 2023;
+    myCar.make = "Toyota";
+    System.out.println("My car is a " + myCar.color + " " + myCar.year + " " + myCar.make);
+  }
+}
 
