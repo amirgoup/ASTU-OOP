0384ed52d0951f911d0d8ec444517c5232e7b535
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-09-17 20:40:38 +0300

    Create DivideByZeroUnhandled.java

---
+++ Lab - 3 : Exception Handling/DivideByZeroUnhandled.java
@@ -0,0 +1,7 @@
+public class DivideByZero {
+    public static void main(String[] args) {
+        int numerator = 10;
+        int denominator = 0;
+        int result = numerator / denominator;
+    }
+}
